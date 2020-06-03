72c6a619f8df26f270f578260a4c92a62daf49b4
NWZX <nwzx@users.noreply.github.com>
2020-06-03 14:46:00 -0400

    Add secret for token management

---
+++ PekockoBack/app/appSettings.ts
@@ -0,0 +1,36 @@
+import crypto from 'crypto';
+import * as fs from 'fs';
+
+export const KEY_SIZE = 64;
+export const SECRET_NAME = 'TOKEN_SECRET';
+
+
+export function randomKey(size: number) {
+    return crypto.randomBytes(size).toString('hex');
+}
+export function getSecret() {
+    let isDockerContain = false;
+    process.argv.forEach(function (val, index, array) {
+        if (index == 2 && val == '--docker')
+            isDockerContain = true;
+    });
+
+    if (isDockerContain && fs.existsSync('/run/secret/' + SECRET_NAME)) {
+        return fs.readFileSync('/run/secret/' + SECRET_NAME, { encoding: 'utf-8' });
+    }
+    else {
+        let pathCompose: string[] = process.argv[1].split('/');
+        pathCompose.pop();
+        pathCompose.push(SECRET_NAME)
+        let path = pathCompose.join('/');
+
+        if (fs.existsSync(path)) {
+            return fs.readFileSync(path, { encoding: 'utf-8' })
+        }
+        else {
+            let key = randomKey(KEY_SIZE);
+            fs.writeFileSync(path, key, { encoding: 'utf-8' });
+            return key;
+        }
+    }
+}
