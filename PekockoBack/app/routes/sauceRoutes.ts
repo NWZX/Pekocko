9ecfac59cd3814b2e2ef0bf896a818fd8e24e8f9
NWZX <nwzx@users.noreply.github.com>
2020-06-04 10:01:53 -0400

    Link sauceRoutes to sauceController

--- PekockoBack/app/routes/sauceRoutes.ts
+++ PekockoBack/app/routes/sauceRoutes.ts
@@ -1,21 +1,21 @@
 import express, { Router } from 'express';
-import * as auth from './security/authentificationModule';
+import * as auth from '../security/authentificationModule';
 import * as sauceController from '../controllers/SauceController';
 
 let router: Router = express.Router();
 
 //Get all sauces
-router.get('/', auth.default, sauceController);
+router.get('/', auth.default, sauceController.GetAllSauce);
 //Add a new sauce
-router.post('/', auth.default, sauceController);
+router.post('/', auth.default, sauceController.AddNewSauce);
 
 //Get a specific sauce
-router.get('/:id', auth.default, sauceController);
+router.get('/:id', auth.default, sauceController.GetSauce);
 //Update a specific sauce
-router.put('/:id', auth.default, sauceController);
+router.put('/:id', auth.default, sauceController.UpdateSauce);
 //Remove a specific sauce
-router.delete('/:id', auth.default, sauceController);
+router.delete('/:id', auth.default, sauceController.DeleteSauce);
 //Update user like status about a specific sauce
-router.post('/:id/like', auth.default, sauceController);
+router.post('/:id/like', auth.default, sauceController.LikeSauce);
 
 export default router;
