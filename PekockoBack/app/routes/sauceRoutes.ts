7e346504e34b1197930cec7126a3b5e25294953f
NWZX <nwzx@users.noreply.github.com>
2020-06-06 20:53:14 -0400

    Add middleware for images upload

--- PekockoBack/app/routes/sauceRoutes.ts
+++ PekockoBack/app/routes/sauceRoutes.ts
@@ -1,18 +1,20 @@
 import express, { Router } from 'express';
+
 import * as auth from '../security/authentificationModule';
 import * as sauceController from '../controllers/SauceController';
+import * as uploadModule from '../security/imgUploadModule';
 
 let router: Router = express.Router();
 
+//Add a new sauce
+router.post('/', auth.default, uploadModule.default, sauceController.AddNewSauce);
 //Get all sauces
 router.get('/', auth.default, sauceController.GetAllSauce);
-//Add a new sauce
-router.post('/', auth.default, sauceController.AddNewSauce);
 
 //Get a specific sauce
 router.get('/:id', auth.default, sauceController.GetSauce);
 //Update a specific sauce
-router.put('/:id', auth.default, sauceController.UpdateSauce);
+router.put('/:id', auth.default, uploadModule.default, sauceController.UpdateSauce);
 //Remove a specific sauce
 router.delete('/:id', auth.default, sauceController.DeleteSauce);
 //Update user like status about a specific sauce
---
+++ PekockoBack/app/security/imgUploadModule.ts
@@ -0,0 +1,73 @@
+import multer from 'multer';
+import * as fs from 'fs';
+
+import { IMG_PATH, randomKey } from '../appSettings';
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, IMG_PATH());
+    },
+    filename: (req, file, callback) => {
+        let name = new Date().getTime() + randomKey(3);
+        const imgPath = IMG_PATH();
+        const ext = file.mimetype.split('/')[1];
+        while (fs.existsSync(imgPath + '/' + name + '.' + ext)) {
+            name += randomKey(3);
+        }
+
+        callback(null, name + '.' + ext);
+    }
+});
+
+/*async function checkFileMIME(file: Express.Multer.File) {
+    try {
+        let array = await toArrayBuffer(file.buffer);
+        if (!(array instanceof ArrayBuffer))
+            throw array;
+
+        let arr = (new Uint8Array(array)).subarray(0, 4);
+        let header = "";
+        for (let i = 0; i < arr.length; i++) {
+            header += arr[i].toString(16);
+        }
+        console.log(header);
+        let type: string = '.' + file.mimetype.split('/')[1];
+        // Add more from http://en.wikipedia.org/wiki/List_of_file_signatures
+        switch (header) {
+            case "89504e47":
+                type = "png";
+                break;
+            case "47494638":
+                type = "gif";
+                break;
+            case "ffd8ffe0":
+            case "ffd8ffe1":
+            case "ffd8ffe2":
+            case "ffd8ffe3":
+            case "ffd8ffe8":
+                type = "jpeg";
+                break;
+            default:
+                // Default file.type
+                break;
+        }
+        return type;
+    } catch (error) {
+        return file.mimetype.split('/')[1];
+    }
+
+}
+async function toArrayBuffer(buf: Buffer) {
+    try {
+        var ab = new ArrayBuffer(buf.length);
+        var view = new Uint8Array(ab);
+        for (var i = 0; i < buf.length; ++i) {
+            view[i] = buf[i];
+        }
+        return ab;
+    } catch (error) {
+        return (error as Error);
+    }
+}*/
+
+export default multer({ storage: storage }).single('image');
