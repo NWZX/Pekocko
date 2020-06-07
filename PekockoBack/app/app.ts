83e44e1ab7a4960915fb21b680af3c7d21bb8b92
NWZX <nwzx@users.noreply.github.com>
2020-06-06 20:54:06 -0400

    Allow CROSS origin

--- PekockoBack/app/app.ts
+++ PekockoBack/app/app.ts
@@ -6,7 +6,7 @@ import mongoose from 'mongoose';
 //Route Files
 import * as userRoutes from './routes/userRoutes';
 import * as sauceRoutes from './routes/sauceRoutes';
-import { ErrorHandler, handleError } from './security/errorModule';
+import { ErrorHandler, handleError, isErrorHandler } from './security/errorModule';
 
 //Setting
 import { PORT, IMG_PATH } from './appSettings';
@@ -23,15 +23,24 @@ mongoose.connect('mongodb://localhost:27017/', { useNewUrlParser: true }).then((
     console.error(error);
 });
 
-app.use(bodyParser.json())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
 
 //Create Route
-app.use('/public/blob', express.static(IMG_PATH()));
+app.use('/public/blob/', express.static(IMG_PATH()));
 app.use('/api/auth', userRoutes.default);
 app.use('/api/sauces', sauceRoutes.default);
 app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
-    if (err instanceof ErrorHandler)
+    if (isErrorHandler(err))
         handleError(err, res);
+    else if (err instanceof Error)
+        handleError(new ErrorHandler(500, '( ' + err.name + ' ) :' + err.message), res);
     else
         handleError(new ErrorHandler(500, JSON.stringify(err)), res);
 });
