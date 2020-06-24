f155e81305c4af4921a29f95160de775d44d51e0
NWZX <nwzx@users.noreply.github.com>
2020-06-24 19:23:10 -0400

    add image resizing middleware

--- PekockoBack/app/routes/sauceRoutes.ts
+++ PekockoBack/app/routes/sauceRoutes.ts
@@ -7,14 +7,14 @@ import * as uploadModule from '../security/imgUploadModule';
 const router: Router = express.Router();
 
 //Add a new sauce
-router.post('/', auth.default, uploadModule.default, sauceController.AddNewSauce);
+router.post('/', auth.default, uploadModule.default, uploadModule.ImgResize, sauceController.AddNewSauce);
 //Get all sauces
 router.get('/', auth.default, sauceController.GetAllSauce);
 
 //Get a specific sauce
 router.get('/:id', auth.default, sauceController.GetSauce);
 //Update a specific sauce
-router.put('/:id', auth.default, uploadModule.default, sauceController.UpdateSauce);
+router.put('/:id', auth.default, uploadModule.default, uploadModule.ImgResize, sauceController.UpdateSauce);
 //Remove a specific sauce
 router.delete('/:id', auth.default, sauceController.DeleteSauce);
 //Update user like status about a specific sauce
--- PekockoBack/app/security/imgUploadModule.ts
+++ PekockoBack/app/security/imgUploadModule.ts
@@ -1,20 +1,23 @@
+import express from 'express';
 import multer from 'multer';
 import * as fs from 'fs';
+import sharp from 'sharp';
 
 import { IMG_PATH, randomKey, IMG_TYPE } from '../appSettings';
 import { ErrorHandler } from './errorModule';
+import path from 'path';
 
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
         callback(null, IMG_PATH());
     },
     filename: (req, file, callback) => {
-        let name = new Date().getTime() + randomKey(3);
+        let name = randomKey(8) + new Date().getTime() + randomKey(8);
         const imgPath = IMG_PATH();
         const ext = file.mimetype.split('/')[1];
         if (IMG_TYPE.includes(ext)) {
             while (fs.existsSync(imgPath + '/' + name + '.' + ext)) {
-                name += randomKey(3);
+                name += randomKey(2);
             }
             callback(null, name + '.' + ext);
         }
@@ -24,55 +27,38 @@ const storage = multer.diskStorage({
     }
 });
 
-/*async function checkFileMIME(file: Express.Multer.File) {
+export async function ImgResize(req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> {
     try {
-        let array = await toArrayBuffer(file.buffer);
-        if (!(array instanceof ArrayBuffer))
-            throw array;
+        if (req.file) {
+            let { filename } = req.file;
 
-        let arr = (new Uint8Array(array)).subarray(0, 4);
-        let header = "";
-        for (let i = 0; i < arr.length; i++) {
-            header += arr[i].toString(16);
+            if (req.file.filename.split('.')[1] != "webp") {
+                filename = filename.split('.')[0] + '.webp';
+                await sharp(req.file.path)
+                    .resize(500)
+                    .webp({ quality: 90 })
+                    .toFile(
+                        path.resolve(req.file.destination, filename)
+                );
+                req.file.filename = filename;
+                req.file.mimetype = "image/webp";
+            }
+            else {
+                await sharp(req.file.path)
+                    .resize(500)
+                    .toFile(
+                        path.resolve(req.file.destination, filename)
+                    );
+            }
+            fs.unlinkSync(req.file.path);
         }
-        console.log(header);
-        let type: string = '.' + file.mimetype.split('/')[1];
-        // Add more from http://en.wikipedia.org/wiki/List_of_file_signatures
-        switch (header) {
-            case "89504e47":
-                type = "png";
-                break;
-            case "47494638":
-                type = "gif";
-                break;
-            case "ffd8ffe0":
-            case "ffd8ffe1":
-            case "ffd8ffe2":
-            case "ffd8ffe3":
-            case "ffd8ffe8":
-                type = "jpeg";
-                break;
-            default:
-                // Default file.type
-                break;
-        }
-        return type;
-    } catch (error) {
-        return file.mimetype.split('/')[1];
+        next();
     }
-
+    catch (error) {
+        if(fs.existsSync(req.file.path))
+            fs.unlinkSync(req.file.path);
+       next(error)
+    }
 }
-async function toArrayBuffer(buf: Buffer) {
-    try {
-        var ab = new ArrayBuffer(buf.length);
-        var view = new Uint8Array(ab);
-        for (var i = 0; i < buf.length; ++i) {
-            view[i] = buf[i];
-        }
-        return ab;
-    } catch (error) {
-        return (error as Error);
-    }
-}*/
 
 export default multer({ storage: storage }).single('image');
