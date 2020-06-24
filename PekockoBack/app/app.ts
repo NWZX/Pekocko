a15811e5f207dbae95e1282b420152cefe6200d8
NWZX <nwzx@users.noreply.github.com>
2020-06-24 19:33:11 -0400

    re-enable limited CROSS domain origin

--- PekockoBack/app/app.ts
+++ PekockoBack/app/app.ts
@@ -24,6 +24,13 @@ mongoose.connect(MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true
     console.error(error);
 });
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 //Helmet http header security
 app.use(helmet());
 app.use(helmet.permittedCrossDomainPolicies());
